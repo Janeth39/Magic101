6261f73f5113557be307103dafba1f1b8abf238e
91093861 <91093861@mvusd.net>
2022-08-22 12:04:50 -0700

    Dragon class

--- src/Dragon.java
+++ src/Dragon.java
@@ -1,9 +1,34 @@
 public class Dragon
 {
     String name;
-    public Dragon(String name)
+    int firePower;
+    public Dragon(String name, int firePower)
     {
+        this.name = name;
+        this.firePower = firePower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFirePower() {
+        return firePower;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
+    public void setFirePower(int firePower) {
+        this.firePower = firePower;
+    }
+
+    @Override
+    public String toString() {
+        return "Dragon{" +
+                "name='" + name + '\'' +
+                ", firePower=" + firePower +
+                '}';
+    }
 }
--- src/WizardDriver.java
+++ src/WizardDriver.java
@@ -2,7 +2,7 @@ public class WizardDriver
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+
     }//end of main
 
 }//end od WizardDriver class
